992ecb2e67d60696667f2fa8a9779a3c87a9b3fe
davidyoon <qqpo12@naver.com>
2021-07-04 16:24:28 +0900

    modify admin page string value on base_site.html(cp from django default package)

--- polls/admin.py
+++ polls/admin.py
@@ -9,8 +9,6 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 
-
-
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
@@ -18,5 +16,8 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
 
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
